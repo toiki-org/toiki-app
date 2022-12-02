2142c49efe503897bef960427dd7a10e340af5b9
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-12-01 23:45:47 -0300

    App setup

---
+++ app/lib/core/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:toiki/utls/constants.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Constants.appName,
+    );
+  }
+}
