ed2c10b86c721f461b993bdecb60920c76ff3635
Elbarae1921 <elbarae1921@gmail.com>
2023-01-01 17:40:13 +0100

    fix(bot): update the API route

--- bot/src/api/convertUrl.ts
+++ bot/src/api/convertUrl.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = process.env.API_URL + '/api/convert-url?url=';
+const API_URL = process.env.API_URL + '/api/convert?url=';
 
 export const convertUrl = async (url: string): Promise<string> => {
   const result = await axios.get(API_URL + url);
-  return result.data.url as string;
-}
+  return result.data.data.url as string;
+};
